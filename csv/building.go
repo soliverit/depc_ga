e4c5dde247b921401e7a14294325a0d29a6221df
soliverit <soliver.i.t@gmail.com>
2023-12-05 22:59:30 +0000

    Retrofit objects and parsing fix
    	Retrofit objects: Before, indices were passed in to select
    	cost/efficiency from Cells[] that the building didn't
    	understand. Now, Retrofits (cost + eff) are stored as objects in
    	the Building.retrofits[] such that a) only the impactful
    	Retrofits are selectable. b) Retrofits are understood by the
    	object. To that end, Building is no longer a fancy CSV Row.
    
    	Linking target data: Original emissions and EPC index are now
    	linked to the Building during prep-time, from the targets.csv
    	file.
    
    	The last value of the Building row (retrofits.csv) wasn't being
    	parsed. It is now.

--- csv/building.go
+++ csv/building.go
@@ -5,26 +5,30 @@ import (
 	"strings"
 )
 
-type Building struct{
+type Building struct {
 	Row
-	cells []float32
+	cells     []float32
+	epcIndex  float32
+	co2       float32
+	retrofits []Retrofit
 }
-func CreateBuilding(lineString string) Building{
-	var building 	Building
-	var tempFloat 	float64
-	var tempString	string
+
+func CreateBuilding(lineString string) Building {
+	var building Building
+	var tempFloat float64
+	var tempString string
 	/*
 		Iterate values converting to float32. Fills blanks with -1
-	 */
-	var cells 		[]float32 = make([]float32,0)
-	var curPos 		uint32= 0
-	var inString	bool = false
+	*/
+	var cells []float32 = make([]float32, 0)
+	var curPos uint32 = 0
+	var inString bool = false
 
 	/*
 		Parse cells
 	*/
 	var stringLength uint32 = uint32(len(lineString))
-	for i := uint32(0); i < stringLength; i++{
+	for i := uint32(0); i < stringLength; i++ {
 		/*
 			Deal with comma encapsulated
 		*/
@@ -33,43 +37,77 @@ func CreateBuilding(lineString string) Building{
 				i += 1
 			}
 			inString = false
-		}else{
-			if lineString[i] == ',' {
-				tempString 		= lineString[curPos:i]
-				if tempString ==""{
-					cells 			= append(cells, -9999)
-				}else {
-					tempFloat, _ 	= strconv.ParseFloat(tempString, 32)
-					cells 			= append(cells, float32(tempFloat))
+		} else {
+			if lineString[i] == ',' || i+1 == stringLength {
+				tempString = lineString[curPos:i]
+				if tempString == "" {
+					cells = append(cells, -9999) //Think this injects -9999 for blank (corrupt) cells
+				} else {
+					tempFloat, _ = strconv.ParseFloat(tempString, 32)
+					cells = append(cells, float32(tempFloat))
 				}
-				curPos 			= 1 + i
-			}else{
-				if lineString[i] == '"'{
+				curPos = 1 + i
+			} else {
+				if lineString[i] == '"' {
 					inString = true
 				}
 			}
 		}
 	}
 	/*
+		Do last value
+	*/
+
+	/*
+		Option headers array: This will hold the retrofit options aliases that
+		have an impact on the Building.
+	*/
+	building.retrofits = make([]Retrofit, 0)
+	building.AddRetrofit(CreateRetrofit("as-built", 0, 0, 0))
+	/*
 		return point and... set cells
-	 */
+	*/
 	building.cells = cells
 	return building
 }
-func(building *Building)Cell(idx int)float32{
+
+/*
+Option Aliases: Filter out the retrofits that do nothing for the building, leaving
+only the labels that have an impact. Uses *-cost.
+*/
+func (building *Building) AddRetrofit(retrofit Retrofit) {
+	building.retrofits = append(building.retrofits, retrofit)
+}
+
+/*
+Getters
+*/
+func (building *Building) EPCIndex() float32         { return building.epcIndex }
+func (building *Building) CO2() float32              { return building.co2 }
+func (building *Building) NumberOfRetrofits() int    { return len(building.retrofits) }
+func (building *Building) Retrofit(id int) *Retrofit { return &building.retrofits[id] }
+
+/*
+Set the original co2, energy efficiency and energy consumption (stored in a separate file from retrofits.csv)
+*/
+func (building *Building) SetOriginalProperties(index, co2 float32) {
+	building.epcIndex = index
+	building.co2 = co2
+}
+func (building *Building) Cell(idx int) float32 {
 	return building.cells[idx]
 }
-func(building *Building)Subtract(value float32, cellIDXs []int){
-	for i := 0; i < len(cellIDXs); i++{
-		if building.cells[cellIDXs[i]] <= 0{
+func (building *Building) Subtract(value float32, cellIDXs []int) {
+	for i := 0; i < len(cellIDXs); i++ {
+		if building.cells[cellIDXs[i]] <= 0 {
 			building.cells[cellIDXs[i]] = building.cells[cellIDXs[i]] - value
 		}
 	}
 }
-func(building *Building)ToString()string{
+func (building *Building) ToString() string {
 	var strCells []string = make([]string, len(building.cells))
-	for i := 0; i < len(building.cells); i++{
-		strCells[i]	= strconv.FormatFloat(float64(building.cells[i]),'f',3,32)
+	for i := 0; i < len(building.cells); i++ {
+		strCells[i] = strconv.FormatFloat(float64(building.cells[i]), 'f', 3, 32)
 	}
 	return strings.Join(strCells, ",")
 }
