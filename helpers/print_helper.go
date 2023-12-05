a52bdf4812d1ad69b1c7959b36e2fc460952b758
soliverit <soliver.i.t@gmail.com>
2023-12-05 23:15:46 +0000

    Created method PrintRow for printing a row of text with constant width Cells

--- helpers/print_helper.go
+++ helpers/print_helper.go
@@ -1,45 +1,60 @@
 package helpers
-import(
+
+import (
 	"os"
 	"strconv"
 	"time"
 )
-type PrintHelper struct{
-	withTimer 	bool
-	currentTime	time.Time
+
+type PrintHelper struct {
+	withTimer   bool
+	currentTime time.Time
 }
-func CreatePrintHelper(withTimer bool)*PrintHelper{
+
+func CreatePrintHelper(withTimer bool) *PrintHelper {
 	var ph PrintHelper
-	ph.withTimer	= withTimer
+	ph.withTimer = withTimer
 	ph.DoDefaultStuff()
 	return &ph
 }
-func(pHelper *PrintHelper) DoDefaultStuff(){
-	if pHelper.withTimer{
+func (pHelper *PrintHelper) DoDefaultStuff() {
+	if pHelper.withTimer {
 		pHelper.currentTime = time.Now()
 	}
 }
-func(pHelper *PrintHelper) P(message string){
+func (pHelper *PrintHelper) P(message string) {
 	var timeString string = ""
-	if pHelper.withTimer{
-		var curTime 		= time.Now()
-		timeString 			= strconv.Itoa(int(curTime.Sub(pHelper.currentTime).Seconds()))
-		pHelper.currentTime	= curTime
+	if pHelper.withTimer {
+		var curTime = time.Now()
+		timeString = strconv.Itoa(int(curTime.Sub(pHelper.currentTime).Seconds()))
+		pHelper.currentTime = curTime
 	}
 	print("\n" + timeString + "\t" + message)
 }
-func PadString(message string, size int)string{
-	for ; len(message) < size;{
+func PadString(message string, size int) string {
+	for len(message) < size {
 		message += " "
 	}
 	return message
 }
+func PrintRow(values []string, length int) {
+	var str string
+	for i := 0; i < len(values); i++ {
+		str += PadString(values[i], length)
+	}
+	println(str)
+}
+
 /*
-	Write message to file
- */
-func(pHelper *PrintHelper)WriteToFile(message string, path string){
+Write message to file
+*/
+func (pHelper *PrintHelper) WriteToFile(message string, path string) {
 	file, _ := os.OpenFile(path,
 		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	file.WriteString(message + "\n")
 	file.Close()
+}
+func (pHealer *PrintHelper) Line(str string) {
+
+	println(str)
 }
