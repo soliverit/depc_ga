40929cecba0f070f2d46396f112dcef96d294573
soliverit <soliver.i.t@gmail.com>
2023-12-11 03:19:15 +0000

    Time-tracking helper. Just records and prints time differences. Very basic

--- helpers/timer.go
+++ helpers/timer.go
@@ -1,1 +1,29 @@
 package helpers
+
+import (
+	"strconv"
+	"time"
+)
+
+type Timer struct {
+	current time.Time
+	last    time.Time
+	records []time.Time
+}
+
+func CreateTimer() Timer {
+	var timer Timer
+	timer.records = make([]time.Time, 0)
+	timer.Reset()
+	return timer
+}
+func (timer *Timer) PrintDiff() {
+	println(strconv.FormatFloat(time.Since(timer.current).Seconds(), 'f', 2, 32))
+}
+func (timer *Timer) Reset() {
+	timer.current = time.Now()
+	timer.last = timer.current
+}
+func (timer *Timer) RecordTime() {
+	timer.records = append(timer.records, time.Now())
+}
